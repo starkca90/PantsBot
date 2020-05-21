7cc3da9c9789a31dbecbaffe29d7966254bc245a
Casey Stark <starkca90@gmail.com>
2020-05-21 14:44:29 -0700

    Continue troubleshooting templating

--- features/webex_cards.js
+++ features/webex_cards.js
@@ -8,7 +8,9 @@ module.exports = function (controller) {
         console.log(message.text.toLowerCase());
         console.log(samplecards[message.text.toLowerCase()]);
 
-        var template = new ACData.Template(samplecards[message.text.toLowerCase]);
+        var temp = samplecards[message.text.toLowerCase];
+
+        var template = new ACData.Template(temp);
 
         var cardPayload = template.expand({
             $root: {
